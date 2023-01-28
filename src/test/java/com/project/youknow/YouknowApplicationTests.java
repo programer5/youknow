c2f2779f53dcdc79022e35be3c44e0c1d8a6a393
programer5 <neverdie4757@gmail.com>
2023-01-28 13:10:41 +0900

    프로젝트 생성

---
+++ src/test/java/com/project/youknow/YouknowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.youknow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouknowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
