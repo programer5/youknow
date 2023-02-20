2c8994d6d9bbbee951c9e98085abaa07e24d1727
programer5 <neverdie4757@gmail.com>
2023-02-21 00:03:34 +0900

    ADD: 시큐리티 설정 정보 작업중

---
+++ src/main/java/com/project/youknow/jwt/JwtAuthProvider.java
@@ -0,0 +1,94 @@
+package com.project.youknow.jwt;
+
+import com.project.youknow.member.entitiy.Member;
+import com.project.youknow.security.CustomUserDetails;
+import io.jsonwebtoken.*;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
+import java.util.Date;
+
+@RequiredArgsConstructor
+@Component
+public class JwtAuthProvider {
+
+    @Value("${spring.jwt.secret.at}")
+    private String atSecretKey;
+
+    @PostConstruct
+    protected void init() {
+        atSecretKey = Base64.getEncoder().encodeToString(atSecretKey.getBytes());
+    }
+
+    private final UserDetailsService userDetailsService;
+
+    /**
+     * @throws Exception
+     * @method 설명: jwt 토큰 발급
+     */
+    public String createToken(Member member) {
+        /**
+         * 토큰 발급을 위한 데이터는 UserDetails에서 담당
+         * 따라서 UserDetails를 세부 구현한 CustomUserDetails로 회원정보 전달
+         */
+        CustomUserDetails user = new CustomUserDetails(member);
+
+        // 유효기간 설정을 위한 Date 객체 선언
+        Date date = new Date();
+        final JwtBuilder builder = Jwts.builder()
+                .setHeaderParam("typ", "JWT")
+                .setSubject("accesstoken").setExpiration(new Date(date.getTime() * (1000L * 60 * 60 * 12)))
+                .claim("member", member)
+                .signWith(SignatureAlgorithm.HS256, atSecretKey);
+
+        return builder.compact();
+    }
+
+    // 토큰에서 회원 정보 추출
+    public String getMemberPK(String token) {
+        return Jwts.parserBuilder().setSigningKey(atSecretKey).build().parseClaimsJws(token).getBody().getSubject();
+    }
+
+    /**
+     * @method 설명: 컨텍스트에 해당 유저에 대한 권한을 전달하고 API 접근 전 접근 권한을 확인하여
+     * 접근 허용 또는 거부를 진행한다.
+     */
+    @SuppressWarnings("unchecked")
+    public Authentication getAuthentication(String token) {
+
+        // 토큰 기반으로 유저의 정보 파싱
+        Claims claims = Jwts.parserBuilder().setSigningKey(atSecretKey).build().parseClaimsJws(token).getBody();
+
+        Member member = claims.get("member", Member.class);
+
+        CustomUserDetails userDetails = new CustomUserDetails(member);
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    /**
+     * @method 설명: request객체 헤더에 담겨 있는 토큰 가져오기
+     */
+    public String resolveToken(HttpServletRequest request) {
+        return request.getHeader("accesstoken");
+    }
+
+    /**
+     * @method 설명: 토큰 유효시간 만료여부 검사 실행
+     */
+    public boolean validateToken(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parserBuilder().setSigningKey(atSecretKey).build().parseClaimsJws(token);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
---
+++ src/main/java/com/project/youknow/security/SpringSecurityConfig.java
@@ -0,0 +1,59 @@
+package com.project.youknow.security;
+
+import com.project.youknow.jwt.JwtAuthProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SpringSecurityConfig {
+
+    private final JwtAuthProvider jwtAuthProvider;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setMaxAge(3600L);
+        configuration.setAllowCredentials(true);
+        configuration.addExposedHeader("accessToken");
+        configuration.addExposedHeader("content-disposition");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web -> web.ignoring().antMatchers("/favicon.ico"));
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+    }
+
+}
+
