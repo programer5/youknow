0182f5948739fe8245efec8d9ef4f8a635fdbdd4
programer5 <neverdie4757@gmail.com>
2023-03-11 21:36:34 +0900

    UPDATE: 시큐리티 설정 수정

--- src/main/java/com/project/youknow/security/SpringSecurityConfig.java
+++ src/main/java/com/project/youknow/security/SpringSecurityConfig.java
@@ -38,7 +38,7 @@ public class SpringSecurityConfig {
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
         configuration.setMaxAge(3600L);
-        configuration.setAllowCredentials(false);
+        configuration.setAllowCredentials(true);
         configuration.addExposedHeader("accessToken");
         configuration.addExposedHeader("content-disposition");
 
